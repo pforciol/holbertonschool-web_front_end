6217a552fb141c3b0656162d62f0d72155ab3a02
Pierre Forcioli <2500@holbertonschool.com>
2021-09-21 16:22:07 +0000

    Add task 10

---
+++ 0x0C-Javascript_advanced/10-prime.js
@@ -0,0 +1,19 @@
+function countPrimeNumbers() {
+    let count = 0;
+    for (let j = 2; j < 100; j++) {
+        let prime = 0;
+        for (var i = 2; i < 100; i++) if (j % i === 0) prime++;
+        if (prime < 2) {
+            count++;
+        }
+    }
+    return count;
+}
+
+var t0 = performance.now();
+for (let i = 0; i < 100; i++) {
+    countPrimeNumbers();
+}
+var t1 = performance.now();
+
+console.log("Execution time of calculating prime numbers 100 times was " + (t1 - t0) + " milliseconds.");
